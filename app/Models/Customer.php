009e68721d3525e19680b01609c35a864ae0ee62
Hazeri <hazeri.sara@gmail.com>
2024-05-09 07:57:42 +0330

    feat(models): add model and enum (try to save phone number in minimum storage by helping Enum)

---
+++ app/Models/Customer.php
@@ -0,0 +1,143 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Enums\CustomerPrefixPhoneNumberEnum;
+use Eghamat24\DatabaseRepository\Models\Entity\Entity;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Customer extends Entity
+{
+    use HasFactory;
+
+    protected int $id;
+
+    protected string $firstName;
+
+    protected string $lastName;
+
+    protected string $dateOfBirth;
+
+    protected CustomerPrefixPhoneNumberEnum $prefixPhoneNumber;
+
+    protected int $phoneNumber;
+
+    protected null|string $email = null;
+
+    protected null|string $bankAccountNumber = null;
+
+    protected null|string $createdAt = null;
+
+    protected null|string $updatedAt = null;
+    protected string $fullPhoneNumber;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getFirstName(): string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(string $firstName): void
+    {
+        $this->firstName = $firstName;
+    }
+
+    public function getLastName(): string
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName(string $lastName): void
+    {
+        $this->lastName = $lastName;
+    }
+
+    public function getDateOfBirth(): string
+    {
+        return $this->dateOfBirth;
+    }
+
+    public function setDateOfBirth(string $dateOfBirth): void
+    {
+        $this->dateOfBirth = $dateOfBirth;
+    }
+
+    public function getEmail(): null|string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(null|string $email): void
+    {
+        $this->email = $email;
+    }
+
+    public function getBankAccountNumber(): null|string
+    {
+        return $this->bankAccountNumber;
+    }
+
+    public function setBankAccountNumber(null|string $bankAccountNumber): void
+    {
+        $this->bankAccountNumber = $bankAccountNumber;
+    }
+
+    public function getCreatedAt(): null|string
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(null|string $createdAt): void
+    {
+        $this->createdAt = $createdAt;
+    }
+
+    public function getUpdatedAt(): null|string
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(null|string $updatedAt): void
+    {
+        $this->updatedAt = $updatedAt;
+    }
+
+    public function getFullPhoneNumber(): string
+    {
+        return $this->getPrefixPhoneNumber()->value . $this->getPhoneNumber();
+    }
+
+    public function setFullPhoneNumber(string $fullPhoneNumber): void
+    {
+        $this->fullPhoneNumber = $fullPhoneNumber;
+    }
+
+    public function getPrefixPhoneNumber(): CustomerPrefixPhoneNumberEnum
+    {
+        return $this->prefixPhoneNumber;
+    }
+
+    public function setPrefixPhoneNumber(CustomerPrefixPhoneNumberEnum $prefixPhoneNumber): void
+    {
+        $this->prefixPhoneNumber = $prefixPhoneNumber;
+    }
+
+    public function getPhoneNumber(): int
+    {
+        return $this->phoneNumber;
+    }
+
+    public function setPhoneNumber(int $phoneNumber): void
+    {
+        $this->phoneNumber = $phoneNumber;
+    }
+}
---
+++ config/repository.php
@@ -0,0 +1,40 @@
+<?php
+
+return [
+
+    'php_version' => env('REPOSITORY_PHP_VERSION', '8.0'),
+
+    'default_db' => 'MySql',# Options: ( Redis, MySql )
+
+    'path' => [
+        'namespace' => [
+            'entities' => 'App\Models\Entities',
+            'enums' => 'App\Models\Enums',
+            'factories' => 'App\Models\Factories',
+            'resources' => 'App\Models\Resources',
+            'repositories' => 'App\Models\Repositories',
+        ],
+
+        'stub' => [
+            'entities' => 'stubs/Entities/entity.',
+            'enums' => 'stubs/Enums/enum.',
+            'factories' => 'stubs/Factories/factory.',
+            'resources' => 'stubs/Resources/resource.',
+            'repositories' => [
+                'base' => 'stubs/Repositories/Base/base.',
+                'mysql' => 'stubs/Repositories/Mysql/mysql.',
+                'interface' => 'stubs/Repositories/Interface/interface.',
+            ]
+        ],
+
+        'relative' => [
+            'entities' => 'app/Models/Entities/',
+            'enums' => 'app/Models/Enums/',
+            'factories' => 'app/Models/Factories/',
+            'resources' => 'app/Models/Resources/',
+            'repositories' => 'app/Models/Repositories/',
+        ],
+
+    ]
+
+];
