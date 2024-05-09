4ee67d66e365c76694481c372040f9010d39b843
Hazeri <hazeri.sara@gmail.com>
2024-05-10 01:29:47 +0330

    fix(test): update until pass TDD test

--- tests/Feature/CustomerTestTDD.php
+++ tests/Feature/CustomerTestTDD.php
@@ -1,45 +1,49 @@
 <?php
 
 
+use App\Models\Enums\CustomerPrefixPhoneNumberEnum;
 use Tests\TestCase;
-use App\Models\Customer;
+use App\Models\Entities\Customer;
 
 class CustomerTestTDD extends TestCase
 {
     public function testCreateCustomer()
     {
+        $email = fake()->unique()->safeEmail();
         $response = $this->postJson('/api/customers', [
-            'firstname' => 'John',
-            'lastname' => 'Doe',
-            'date_of_birth' => '1990-01-01',
-            'prefix_phone_number' => '+1',
-            'phone_number' => '234567890',
-            'email' => 'john@example.com',
-            'bank_account_number' => '1234567890'
+            'first_name' => fake()->name(),
+            'last_name' => fake()->lastName(),
+            'date_of_birth' => fake()->date(),
+            'prefix_phone_number' => fake()->randomElement(array_column(CustomerPrefixPhoneNumberEnum::cases(), 'value')),
+            'phone_number' => fake()->numerify('##########'),
+            'email' => $email,
+            'bank_account_number' => fake()->creditCardNumber()
         ]);
 
         $response->assertStatus(201);
-        $this->assertDatabaseHas('customers', ['email' => 'john@example.com']);
+        $this->assertDatabaseHas('customers', ['email' => $email]);
     }
 
     public function testReadCustomer()
     {
+        $email = fake()->unique()->safeEmail();
+        $firstname = fake()->name();
         $customer = Customer::factory()->create([
-            'firstname' => 'Jane',
-            'lastname' => 'Doe',
-            'date_of_birth' => '1995-05-05',
-            'prefix_phone_number' => '+98',
-            'phone_number' => '76543210',
-            'email' => 'jane@example.com',
-            'bank_account_number' => '0987654321'
+            'first_name' => $firstname,
+            'last_name' => fake()->lastName(),
+            'date_of_birth' => fake()->date(),
+            'prefix_phone_number' => fake()->randomElement(array_column(CustomerPrefixPhoneNumberEnum::cases(), 'value')),
+            'phone_number' => fake()->numerify('##########'),
+            'email' => $email,
+            'bank_account_number' => fake()->creditCardNumber()
         ]);
 
-        $response = $this->getJson('/api/customers/' . $customer->getId());
+        $response = $this->getJson('/api/customers/' . $customer->id);
 
         $response->assertStatus(200)
             ->assertJson([
-                'firstname' => 'Jane',
-                'email' => 'jane@example.com'
+                'firstName' => $firstname,
+                'email' => $email
             ]);
     }
 
@@ -47,22 +51,27 @@ class CustomerTestTDD extends TestCase
     {
         $customer = Customer::factory()->create();
 
-        $response = $this->putJson('/api/customers/' . $customer->getId(), [
-            'firstname' => 'UpdatedFirstName',
-// Update other fields as needed
+        $response = $this->putJson('/api/customers/' . $customer->id, [
+            'first_name' => 'UpdatedFirstName',
+            'last_name' => $customer->last_name,
+            'date_of_birth' => $customer->date_of_birth,
+            'prefix_phone_number' => CustomerPrefixPhoneNumberEnum::getEnumByName($customer->prefix_phone_number),
+            'phone_number' => $customer->phone_number,
+            'email' => $customer->email,
+            'bank_account_number' => $customer->bank_account_number
         ]);
 
         $response->assertStatus(200);
-        $this->assertDatabaseHas('customers', ['id' => $customer->getId(), 'firstname' => 'UpdatedFirstName']);
+        $this->assertDatabaseHas('customers', ['id' => $customer->id, 'first_name' => 'UpdatedFirstName']);
     }
 
     public function testDeleteCustomer()
     {
         $customer = Customer::factory()->create();
 
-        $response = $this->deleteJson('/api/customers/' . $customer->getId());
+        $response = $this->deleteJson('/api/customers/' . $customer->id);
 
         $response->assertStatus(204);
-        $this->assertDatabaseMissing('customers', ['id' => $customer->getId()]);
+        $this->assertDatabaseMissing('customers', ['id' => $customer->id]);
     }
 }
